a2bf1d09929c41fa545cf35af077e96f47056295
Gabriel <gabrielvasconcelosv21@gmail.com>
2023-06-17 12:38:18 -0300

    Aula04

--- src/App.js
+++ src/App.js
@@ -5,11 +5,24 @@ function App() {
   return (
     <div className="App">
       <div className='app__videos'> 
-        <Video />
-        <Video />
-        <Video />
-        <Video />
-        <Video />
+        <Video 
+          likes={111}
+          messages={222}
+          shares={333}
+          name="Gabriel"
+          description="Gato Saruê"
+          music="Musica incrivelmente insana"
+          url="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4?t=2023-05-22T19%3A37%3A45.885Z"
+        />
+        <Video 
+          likes={999}
+          messages={888}
+          shares={777}
+          name="Gublas"
+          description="Ser pensante"
+          music="Clapi Clapi"
+          url="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/bird.mp4?t=2023-05-22T19%3A40%3A47.052Z"
+        />
       </div>
     </div>
   );
